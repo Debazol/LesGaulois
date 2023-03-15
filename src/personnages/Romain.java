aff080eb788ca03027f0b84b6fc85134bf7ce8f0
Debazol <125345131+Debazol@users.noreply.github.com>
2023-03-15 14:26:47 +0100

    invariant pre post conditions

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -5,6 +5,7 @@ public class Romain {
 	private int force;
 
 	public Romain(String nom, int force) {
+		assert (force>0)==true;
 		this.nom = nom;
 		this.force = force;
 	}
@@ -13,7 +14,7 @@ public class Romain {
 	}
 	
 	public void parler(String texte) {
-		System.out.println(prendreParole() + "« " + texte + "»");
+		System.out.println(prendreParole() + "<< " + texte + ">>");
 	}
 	
 	private String prendreParole() {
@@ -21,20 +22,27 @@ public class Romain {
 	}
 	
 	public void recevoirCoup(int forceCoup) {
+		int debut=force;
+		assert (debut>0)==true;
 		force -= forceCoup;
 		if (force > 0) {
-			parler("Aïe");
-	} 
+			parler("Aie");
+			} 
 		else {
 			parler("J'abandonne...");
-	}
+			}
+		int fin=force;
+		assert (debut>fin)==true;
+	
 	}
 	public static void main(String[] args) {
 		Romain cesar= new Romain("cesar", 4);
 		System.out.println(cesar.getNom());
 //		cesar.parler("bonjour");
-//		cesar.recevoirCoup(2);
-//		cesar.recevoirCoup(5);
+		cesar.recevoirCoup(2);
+		cesar.recevoirCoup(5);
+		
+		Romain minus= new Romain("minus", 6);
 		
 	}
 
